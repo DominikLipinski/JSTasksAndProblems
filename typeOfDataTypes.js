143b98926e1b94b38583827865b4eb6bf2a74423
DominikLipinski <maboonga@gmail.com>
2018-04-27 21:32:30 +0200

    typeof data types

---
+++ typeOfDataTypes.js
@@ -0,0 +1,13 @@
+/* Whats logged into console with each statement */
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof {}); // object
+console.log(typeof []); // object
+
+/* 
+How to check if some object is an array?
+
+console.log(Array.isArray([]));
+console.log([] instanceof Array);
+*/
